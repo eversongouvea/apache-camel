afe0c8727b1bcd9260114733cc9f767dc8230cb1
Everson Gouvea <suporte.gouvea@gmail.com>
2019-09-14 21:21:32 -0300

    Adicionado rota soap

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -17,16 +17,31 @@ public class RotaPedidos {
 			public void configure() throws Exception {
 				
 				from("file:pedidos?delay=5s&noop=true")
-				.split()
-					.xpath("/pedido/itens/item")
-				.filter()
-					.xpath("item/formato[text()='EBOOK']")
-				.log("${id}")
+				.routeId("rota-pedidos")
+				.multicast()
+				.to("direct:http")
+				.to("direct:soap");
+				
+				from("direct:http")
+				.routeId("rota-http")
+				.setProperty("pedidoId",xpath("/pedido/id/text()"))
+				.setProperty("clienteId",xpath("/pedido/pagamento/email-titular/text()"))
+				.split().xpath("/pedido/itens/item")
+				.filter().xpath("item/formato[text()='EBOOK']")
+				.setProperty("ebookId",xpath("/item/livro/codigo/text()"))
+				//.log("${id}")
 				.marshal().xmljson()
-				.log("${body}")
+				//.log("${body}")
 				.setHeader(Exchange.HTTP_METHOD,HttpMethods.GET)
-				.setHeader(Exchange.HTTP_QUERY,simple("pedidoId=123"))
+				.setHeader(Exchange.HTTP_QUERY,simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}"))
 				.to("http4://localhost:9095/ebooks");
+				
+				from("direct:soap")
+				.routeId("rota-soap")
+				.to("xslt:pedido-para-soap.xslt")
+				.log("${body}")
+				.setHeader(Exchange.CONTENT_TYPE,constant("text/xml"))
+				.to("http4://localhost:8080/webservices/financeiro");
 				
 			}
 		});
